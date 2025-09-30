dac36df6bb7fe948e3e26eb6a18ca1317391e91d
Aisyah Septiani <aisyahseptiani20@gmail.com>
2025-09-30 22:46:14 +0700

    feat: Complete Convex Hull assignment

---
+++ latihan1/drawingcanvas.h
@@ -0,0 +1,64 @@
+#ifndef DRAWINGCANVAS_H
+#define DRAWINGCANVAS_H
+
+#include <QWidget>
+#include <QPoint>
+#include <QVector>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QtMath>
+#include <algorithm>
+#include <set>
+
+class DrawingCanvas : public QWidget
+{
+    Q_OBJECT
+
+public:
+    DrawingCanvas(QWidget *parent = nullptr);
+
+    enum AlgorithmType {
+        SlowHull,
+        FastHull,
+        JarvisMarch
+    };
+    Q_ENUM(AlgorithmType)
+
+    void computeConvexHull(AlgorithmType type);
+    void clearCanvas();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    // Comparator untuk QPoint agar bisa digunakan di std::set (untuk Brute Force)
+    struct QPointLess {
+        bool operator()(const QPoint& a, const QPoint& b) const {
+            if (a.x() != b.x()) {
+                return a.x() < b.x();
+            }
+            return a.y() < b.y();
+        }
+    };
+
+    QVector<QPoint> m_points;
+    QVector<QPoint> m_hull;
+    int m_iterations = 0;
+    QString m_algorithmName = "";
+
+    // Helper Functions (Semua harus static karena dipanggil dari comparePoints yang static)
+    static int orientation(QPoint p, QPoint q, QPoint r);
+    static qreal distSq(QPoint p1, QPoint p2);
+
+    // Algoritma
+    void runBruteForce();
+    void runGrahamScan();
+    void runJarvisMarch();
+
+    // Untuk Graham Scan
+    static QPoint p0;
+    static bool comparePoints(const QPoint& a, const QPoint& b);
+};
+
+#endif // DRAWINGCANVAS_H
